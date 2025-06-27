dfea1269791ca4db71e2219ebc283e4b85677063
Cornation <2669503659@qq.com>
2025-06-27 17:17:32 +0800

    app

--- pages/his/login/center.vue
+++ pages/his/login/center.vue
@@ -7,7 +7,6 @@
 						<view class="avator">
 							<img class="logo-img" :src="hasLogin ? logUrl : logoutUrl">
 						</view>
-						<!--  -->
 						<view class="phone-number">
 							<text class="uer-name">{{hasLogin ? "Hi，"+name : '您尚未登录'}}</text>
 						</view>
